09b31363f4e52664199e85ccbc22f82ea91484f7
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2021-04-24 20:12:17 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -43,6 +43,9 @@ input.onGesture(Gesture.Shake, function () {
     rooms[room].showImage(0)
     led.toggle(Xpos(pos), Ypos(pos))
 })
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music.setBuiltInSpeakerEnabled(true)
+})
 function Ypos (num: number) {
     y = Math.trunc(num / 5)
     return y
@@ -54,39 +57,28 @@ function Xpos (num: number) {
     x = num % 5
     return x
 }
-basic.showIcon(IconNames.StickFigure)
-basic.showIcon(IconNames.Diamond)
-let x: number = 0
-let y: number = 0
-let room: number = 0
-let newpos: number = 0
-let Treas: number = 0
-let Ex: number = 0
+let x = 0
+let y = 0
+let room = 0
+let newpos = 0
+let Treas = 0
+let Ex = 0
 let Dirs: number[] = []
 let doors: number[] = []
-let pos: number = 0
-let dir: number = 0
-let rooms: Image[] = []
-let Room1 = images.createImage(`
+let pos = 0
+let dir = 0
+let rooms: number[] = []
+music.setBuiltInSpeakerEnabled(false)
+basic.showIcon(IconNames.StickFigure)
+basic.showIcon(IconNames.Diamond)
+let Room1 = 0
+let Room2 = 0
+let Room3 = images.createImage(`
     . # # # #
     . # # . #
     . . # . #
     . . . . #
     # . # . .
-    `)
-let Room2 = images.createImage(`
-    # # # # .
-    # # # . .
-    . . # . #
-    . . . . #
-    # . # . .
-    `)
-let Room3 = images.createImage(`
-    # # # # #
-    # # . . .
-    # . # . #
-    # . . . #
-    # . # # .
     `)
 let Room4 = images.createImage(`
     # # # # #
@@ -105,16 +97,14 @@ let Room5 = images.createImage(`
 rooms = [Room1, Room2, Room3, Room4, Room5]
 dir = 2
 pos = 0
-doors = [0, 24, 10, 4, 21, 7, 7 , 18, 18, 0]
+doors = [0, 24, 10, 4, 21, 7, 7, 18, 18, 0]
 Dirs = [1, 6, 5, 4, -1, -6, -5, -4]
 music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
 EnterRoom(0)
-
 basic.forever(function () {
     if (pos == Ex) {
-        game.setScore(game.score()+Treas)
+        game.setScore(game.score() + Treas)
         Treas = 1000
-        
         room += 1
         if (room == 5) {
             music.startMelody(music.builtInMelody(Melodies.Nyan), MelodyOptions.Once)
